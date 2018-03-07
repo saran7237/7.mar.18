de02ddbeede1c0795a5ce4924da11344b1f47811
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-03-07 15:47:18 +0530

    Create 3pro.7.3.c

---
+++ 3pro.7.3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+	int a,b,c;
+	printf("enter the number:");
+	scanf("%d",&a);
+	printf("enter the number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d",c);
+	return 0;
+getch();
+}
