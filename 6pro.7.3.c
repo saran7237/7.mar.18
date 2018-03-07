60a2d94746bca56386a9cef6c830e5f68c71f833
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-03-07 15:54:19 +0530

    Create 6pro.7.3.c

---
+++ 6pro.7.3.c
@@ -0,0 +1,18 @@
+def switch_demo(argument):
+    switcher = {
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+        10: "Ten"
+    }
+    print switcher.get(argument, "Invalid Input")
+n=int(raw_input())
+}
+return 0;
+}
